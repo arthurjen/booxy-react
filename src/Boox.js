c807527f3e1b1a495ad39f19e2c4a76f88480112
Arthur Jen <arthurljen@gmail.com>
2018-10-03 11:57:38 -0700

    booxy works! without transitions

--- src/Boox.js
+++ src/Boox.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import styles from './Boox.css';
 
 class Boox extends Component {
-  state = {  }
+  state = {
+    clicked: false
+  };
+
   render() { 
+    const { clicked } = this.state;
     return (
-      <p>box</p>
+      <div
+        className={styles.boox}
+        style={clicked ? { background: 'white' } : null}
+        onClick={() => this.setState({ clicked: true })}
+      >
+        {
+          clicked &&
+          [...Array(4)].map((n, i) => (
+            <Boox key={i}/>
+          ))
+        }
+      </div>
     );
   }
 }
